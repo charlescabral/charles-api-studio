1f74d91b8695c6461ff33753f84497023f00bc5b
Charles Cabral <charles.cabral.neto@gmail.com>
2025-05-24 17:47:07 -0300

    update

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   return res.json({
     name: "Data Validation Studio",
-    description: "Um laboratório de APIs para validação de dados",
+    description: "Laboratório de APIs para validação de dados",
     version: "1.0.0",
     status: "online",
   });
